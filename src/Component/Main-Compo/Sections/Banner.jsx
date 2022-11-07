55153a692b5b5ea3a41727b5f1439c2f551d6930
kobir0 <kxkobir1999@gmail.com>
2022-11-08 03:26:24 +0600

    Correction

--- src/Component/Main-Compo/Sections/Banner.jsx
+++ src/Component/Main-Compo/Sections/Banner.jsx
@@ -5,11 +5,11 @@ const Banner = () => {
     <div>
       <div className="hero min-h-screen ">
         <div className="flex items-center justify-center flex-col lg:flex-row">
-          <div className="w-full flex justify-center my-10 lg:my-0 lg:w-2/5">
+          <div className="w-full flex justify-center my-0 lg:my-8  lg:w-2/5">
             <img
               src="https://i.ibb.co/Z2WxR0J/107925-doctor.gif"
               alt=""
-              className=" rounded-lg shadow-2xl"
+              className=" rounded-lg "
             />
           </div>
           <div className="mx-10 flex justify-center">
@@ -18,18 +18,18 @@ const Banner = () => {
                 Hi, I am{" "}
                 <span className="text-teal-500">Doc. Edward Jenner </span>.
               </h1>
-              <p className="py-6">
+              <p className="py-6 font-semibold">
                 Access to the health care may vary across countries ,
                 communities , and social and economic <br></br> conditions as
                 well as health policies .
               </p>
               <div className="flex">
                 {" "}
-                <button className="btn  lg:btn-wide btn-outline btn-success m-2">
-                  Get Appoinment
-                </button>
-                <button className="btn lg:btn-wide  m-2 btn-success">
+                <button className="btn shadow-xl shadow-teal-100 lg:btn-wide btn-outline m-2 btn-success">
                   See Services
+                </button>
+                <button className="btn shadow-xl shadow-teal-100 lg:btn-wide  btn-success m-2">
+                  Get Appointment
                 </button>
               </div>
             </div>
