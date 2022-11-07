ce872cb1bf4e2012f5a54ed45acc632f13d6348f
kobir0 <kxkobir1999@gmail.com>
2022-11-08 04:08:05 +0600

    User Context

--- src/Component/Shared-Compo/Routes.jsx
+++ src/Component/Shared-Compo/Routes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../Authentication/Login";
+import Register from "../Authentication/Register";
 import Home from "../Main-Compo/Home";
 import Main from "../Main-Compo/Main";
 import Services from "../Main-Compo/Services";
@@ -20,6 +22,14 @@ const Routes = createBrowserRouter([
       {
         path: "/services",
         element: <Services></Services>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
       },
     ],
   },
---
+++ src/Component/Shared-Compo/UserContext.jsx
@@ -0,0 +1,72 @@
+import React, { createContext, useEffect, useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import app from "./Firebase.config";
+
+export const AuthContext = createContext();
+
+const UserContext = ({ children }) => {
+  const googleProvider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const updateProfileInfo = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
+
+  const logIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const signInWithPopGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const AuthInfo = {
+    updateProfileInfo,
+    loading,
+    logOut,
+    user,
+    auth,
+    createUser,
+    logIn,
+    signInWithPopGoogle,
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (createUser) => {
+      setUser(createUser);
+      console.log(createUser);
+      setLoading(false);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  return (
+    <div>
+      <AuthContext.Provider value={AuthInfo}>{children}</AuthContext.Provider>
+    </div>
+  );
+};
+
+export default UserContext;
